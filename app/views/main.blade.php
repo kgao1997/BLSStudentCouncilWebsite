d7d9880319c2a4445f138b4d0fcfe21567b0c6c8
kgao1997 <kgao1997@gmail.com>
2014-08-16 16:33:39 -0400

    first commit

---
+++ app/views/main.blade.php
@@ -0,0 +1,20 @@
+@extends('master')
+@section('title')
+  BLSSC!
+@stop 
+
+@section('header')
+  <h1> 
+  Welcome to the Boston Latin School Student Council!
+  </h1>
+@stop
+
+@section('content')
+  <h1> 
+  Meeting news!
+  </h1>
+
+  <p>
+  The First meeting will be September 8th after school (9/8/2014)
+  </p>
+@stop
---
+++ app/views/master.blade.php
@@ -0,0 +1,19 @@
+
+<!doctype html>
+<html>
+<meta-charset = "utf-8">
+@section('title', 'default title')
+<head>
+<title>
+@yield('title')
+</title>
+</head>
+
+@section('header', 'default header')
+@yield('header')
+
+@section('content', 'default content')
+@yield('content')
+
+
+  
