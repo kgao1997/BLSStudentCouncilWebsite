36fb63b37e8fd5ff48fac752ef69b5ebc36478fb
kgao1997 <kgao1997@gmail.com>
2014-09-08 18:10:32 -0400

    Added images and fixed officers

--- app/views/aboutUs.blade.php
+++ app/views/aboutUs.blade.php
@@ -4,11 +4,14 @@
 
 <br>
 Officers<br>
-President:  Angie Hourihan<br>
+President:  Angela Hourihan<br>
 Vice-President:  Gloribel Rivas<br>
-Secretary:  <br>
-Treasurer:  <br>
+Secretary:  Christina Yee  <br>
+Treasurer:  Ana Battaglino  <br>
+External Liasions:  Haben Kelati and Randy Chen  <br>
 IT Director:  Kevin Gao<br>
 <br>
-If you have questions, concerns, or just simply want to get involved in the Student Council, please email  blsstudentcouncil@gmail.com.  We'll add you to our mailing list and we promise to get back to you ASAP
+If you have questions, concerns, or just simply want to get involved in the Student Council, please email  blsstudentcouncil@gmail.com.  We'll add you to our mailing list and we promise to get back to you ASAP<br>
+<br>
+
 @stop
--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -1,7 +1,7 @@
 @extends('master')
 
 @section('content') 
-
-
+<div align="center">
+<img src="http://s1.postimg.org/v338g7unh/Student_Council_1.png" alt="Keep Calm and Join Student Council" style="width:500px;height:500px;">
 
 @stop
