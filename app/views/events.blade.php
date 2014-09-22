b01838ffd1a6985220704cee239a9a9245b2c769
kgao1997 <kgao1997@gmail.com>
2014-09-22 16:09:02 -0400

    updated dates

--- app/views/events.blade.php
+++ app/views/events.blade.php
@@ -3,7 +3,19 @@
 @section('content')
 <div align="center">
 Events this year:<br>
-Class VI Forum-  Will be hosted in October<br>
+Extracurricular Fair:  9/24  <br>
+<br>
+Come visit us at the extracurricular fair and sign up for the Student Council!<br>
+Class VI Forum-  October 20th<br>
+The Class VI Forum is meant for members of class VI to meet and interact with older members of the BLS <br>
+community and learn more about what its like to be a BLS student, as well as learn what to expect in the <br>
+coming six years.<br>
+<br>
+Open House- November 3rd<br>
+The Open House is an opportunity for all members of the BLS community to look into joining the Student Council,<br>
+find out what we do, or just voice their thoughts.  Come join us!<br>
+<br>
+  
 Globe Santa Drive-  Will be in December<br>
 Student-Faculty Basketball Game- Will be in March-April<br>
 Crystal Apple Awards- June<br>
