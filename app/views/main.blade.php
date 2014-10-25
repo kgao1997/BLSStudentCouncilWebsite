8d77f97e34dd5229bd1625c075ab4a0ba9f38545
kgao1997 <kgao1997@gmail.com>
2014-10-25 12:43:27 -0400

    fixed image

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -1,7 +1,7 @@
 @extends('master')
 
 @section('content') 
-<div align="center">
-<img src="http://s1.postimg.org/v338g7unh/Student_Council_1.png" alt="Keep Calm and Join Student Council" style="width:500px;height:500px;">
+<div align="">
+<img src="http://i36.photobucket.com/albums/e33/kevin_gao1/StudentCouncil1_zps83d7f170.png" alt=" photo StudentCouncil1_zps83d7f170.png" style = "height:500px; width:500px; margin-left:190px;" >
 
 @stop
