815023eb0367d1155e20bd43466098e33cb0f0f2
kgao1997 <kgao1997@gmail.com>
2014-11-09 23:57:10 -0500

    updated news)

--- app/views/news.blade.php
+++ app/views/news.blade.php
@@ -2,7 +2,9 @@
 
 @section('content')
 <div align="center">
-
-No News to report currently.  Keep checking for updates!
+There will be no Student Council Meeting tomorrow (11/10/2014)<br>
+<br>
+<br>
+The next meeting will be Monday, 11/17/2014
 
 @stop
