1166ba6880db0debc66a1331da8532f4cbad98cb
kgao1997 <kgao1997@gmail.com>
2014-11-09 23:50:06 -0500

    added mission statement

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -1,7 +1,19 @@
 @extends('master')
 
 @section('content') 
-<div align="">
-<img src="http://i36.photobucket.com/albums/e33/kevin_gao1/StudentCouncil1_zps83d7f170.png" alt=" photo StudentCouncil1_zps83d7f170.png" style = "height:500px; width:500px; margin-left:190px;" >
+<div align="center", font size="24";>
+<h1>Mission Statement</h1><br>
+<br>
+We seek to provide an open forum for students concerns and ideas for improving Boston Latin School<br>
+in order to make it a more effective and enjoyable learning environment for all.<br>
+We seek to be the voice of the students of Boston Latin School<br
+and to represent them and their needs in our relationship with the school administration.<br>
+We seek to train and install a network of leaders that work together,<br>
+gaining leadership abilities and skills while sponsoring events <br>
+and working to solve the problems that face their fellow classmates. <br>
+<br>
+<br>
+<br>
+<img src="http://i36.photobucket.com/albums/e33/kevin_gao1/StudentCouncil1_zps83d7f170.png" alt=" photo StudentCouncil1_zps83d7f170.png" style = "height:200px; width:200px;margin-right = 300px" >
 
 @stop
