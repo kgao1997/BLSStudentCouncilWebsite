4909642a3ede10abc74031673cff905b4b67f58d
kgao1997 <kgao1997@gmail.com>
2014-11-11 23:06:08 -0500

    added twitter feed

--- app/views/main.blade.php
+++ app/views/main.blade.php
@@ -1,7 +1,13 @@
 @extends('master')
 
 @section('content') 
-<div align="center", font size="24";>
+<div align=""; font size="24";>
+
+<div id = "Our Goals"; style =
+"width:400px;
+ height:600px;
+ margin-left: 50px;
+ float: left">
 <h1>Mission Statement</h1><br>
 <br>
 We seek to provide an open forum for students concerns and ideas for improving Boston Latin School<br>
@@ -11,9 +17,14 @@ and to represent them and their needs in our relationship with the school admini
 We seek to train and install a network of leaders that work together,<br>
 gaining leadership abilities and skills while sponsoring events <br>
 and working to solve the problems that face their fellow classmates. <br>
-<br>
-<br>
-<br>
-<img src="http://i36.photobucket.com/albums/e33/kevin_gao1/StudentCouncil1_zps83d7f170.png" alt=" photo StudentCouncil1_zps83d7f170.png" style = "height:200px; width:200px;margin-right = 300px" >
+</div>
+
+<div id = "Twitter Feed"; style = 
+" float: right;
+  margin-right: 50px;
+";>
+<a class="twitter-timeline" width="300"; height="500"; href="https://twitter.com/BLSStuCo" data-widget-id="532366160193269760">Tweets by @BLSStuCo</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
 
 @stop
